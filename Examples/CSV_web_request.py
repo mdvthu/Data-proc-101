98b27316e350f5005550d5e94450b26cc6ddc6c6
Mark Thurston <mark@mdvthu.com>
2018-03-25 20:56:38 +0100

    Add regex feature to CSV search

--- Examples/CSV_web_request.py
+++ Examples/CSV_web_request.py
@@ -1,11 +1,30 @@
 #!/usr/bin/env python3
 
 import requests
+# the regular expressions library
+import re
 
+# location of the document to get
 url = "http://data.gov.uk/data/resource/nhschoices/Hospital.csv"
 result = requests.get(url)
 
+# break the result in to lines (\r\n indicates the end of a line)
 processed = result.text.split('\r\n')
+# split each line at the tab character (\t)
 processed[:] = [ j.split('\t') for j in processed ]
 
-print(processed[0])
+print("Enter the start of a postcode: ", end="")
+postcode = input()
+print("")
+
+# complie a regular expression
+r = re.compile("^" + postcode, re.IGNORECASE)
+
+# go through each line in the data
+for line in processed:
+    # if the pattern matches, print the hospital and postcode, then a newline
+    if r.match(line[13]):
+        print(line[7])
+        print(line[13])
+        print("")
+
